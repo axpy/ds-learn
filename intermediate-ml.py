2f9a91a68557a31e3762bc7d35ccdee2b2baaba6
Eugene Crytal <axpycore@gmail.com>
2020-09-15 19:17:39 +0300

    Add drop and local encoding

--- intermediate-ml.py
+++ intermediate-ml.py
@@ -1,4 +1,6 @@
+# **********************************
 # **********MISSING VALUES**********
+# **********************************
 
 # Many libraries will throw errors in case of missing values, so data should be prepared.
 # Drop columns (simply remove column)
@@ -124,4 +126,122 @@ final_X_valid = pd.DataFrame(final_imputer.transform(reduced_X_valid))
 
 final_X_train.columns = reduced_X_train.columns
 final_X_valid.columns = reduced_X_train.columns
+
+
+
+# *****************************************
+# **********CATEGORICAL VARIABLES**********
+# *****************************************
+
+# Transform strings into numbers or something
+
+s = (X_train.dtypes == 'object')
+object_cols = list(s[s].index)
+# .dtypes == list of columns; (...) === create generator for tuple with columns from X_train which are 'object'
+
+# --------------------Drop Categorical Variables--------------------
+# Easiest and the worst one
+
+drop_X_train = X_train.select_dtypes(exclude=['object'])
+
+# --------------------Label Encoding--------------------
+# Transform text into numbers. Works well with ordinal data, e.g time variables, or any measurements.
+# Breakfast   Breakfast
+# Every day   3
+# Never       0
+# Rarely      1
+
+from sklearn.preprocessing import LabelEncoder
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+label_encoder = LabelEncoder()
+for col in object_cols:
+    label_X_train[col] = label_encoder.fit_transform(X_train[col])
+    label_X_valid[col] = label_encoder.transform(X_valid[col])
+# Example of generating random values. Ideally it should be done manually, as numbers' values matter
+
+# --------------------One-Hot Encoding--------------------
+# Create new column for each possible categorical variable, and then insert appropriate number
+# Color    Red  Yellow  Green
+# Red      1    0       0
+# Red      1    0       0
+# Yellow   0    1       0
+# Green    0    0       1
+
+
+# --------------------------------------------------------------------------------
+# Label Encoding and One-Hot Encoding in action 
+# --------------------------------------------------------------------------------
+
+from sklearn.preprocessing import OneHotEncoder
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False) # handle_unknown - unknown values (for which was not created column), that may encounter in validation data; sparse=False - encoded columns are returned as numpy array, instead of spare matrix
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Drop and then add back encoded columns
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+
+# Label encoding examlpe
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+X = pd.read_csv('../input/train.csv', index_col='Id')
+X_test = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X.SalePrice
+X.drop(['SalePrice'], axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y,
+                                                      train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+X_train['Condition2'].unique() !== X_valid['Condition2'].unique()
+# There's a problem. Train and valid data frames may have different labels.
+
+# train Condition2:    valid Condition2:
+# aaa                  bbb
+# bbb                  ddd
+# aaa                  xxx
+# ccc                  aaa
+
+# Simplest solution - drop "ugly" columns, and encode "good" ones
+object_cols = [col for col in X_train.columns if X_train[col].dtype == "object"] # All object cols
+good_label_cols = [col for col in object_cols if set(X_train[col]) == set(X_valid[col])] # Cols which has exact same sets of categorized values 
+bad_label_cols = list(set(object_cols)-set(good_label_cols)) # Remove from all calls good one, and leave only bad
+
+from sklearn.preprocessing import LabelEncoder
+label_X_train = X_train.drop(bad_label_cols, axis=1)
+label_X_valid = X_valid.drop(bad_label_cols, axis=1)
+
+# Apply label encoder 
+label_encoder = LabelEncoder()
+for col in good_label_cols:
+    label_X_train[col] = label_encoder.fit_transform(X_train[col])
+    label_X_valid[col] = label_encoder.transform(X_valid[col])
+
+
+
+
+
+
+
+
+
+
+
+
 
